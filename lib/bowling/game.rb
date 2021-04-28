2be3db043d45e4e1ea5c4f5ad01a2f78a199d627
Igor S. Morozov <igor@morozov.is>
2021-04-29 02:25:02 +0300

    Remove LastFrame class

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -9,24 +9,30 @@ module Bowling
       return :too_many_pins if pins > 10
 
       case frame
+      # Any last throw is recorded as-is, even if it's a strike
+      in Frame(balls: [], serial_number: 10)
+        frame.new(balls: [pins])
+      # Even if the first ball of the last frame was a strike, we'll accept anything
+      in Frame(balls: [10], serial_number: 10)
+        frame.new(balls: [10, pins])
+      # A spare doesn't end the last frame
+      in Frame(balls: [ball], serial_number: 10) if ball + pins == 10
+        frame.new(balls: [ball, pins])
+      # Anything which is not a first ball, a strike or a spare, ends the frame
+      in Frame(balls: balls, serial_number: 10)
+        frame.new(balls: [*balls, pins], state: 'ended')
+      # Strike ends the frame
       in Frame(balls: []) if pins == 10
         frame.new(balls: [pins], state: 'ended')
+      # Can't knock down more pins than possible
       in Frame(balls: [ball]) if pins + ball > 10
         :too_many_pins
+      # Second ball ends the game
       in Frame(balls: [ball])
         frame.new(balls: [ball, pins], state: 'ended')
+      # First ball which is not a strike is recorded as-is
       in Frame(balls: [])
         frame.new(balls: [pins])
-      in LastFrame(balls: [])
-        frame.new(balls: [pins])
-      in LastFrame(balls: [10])
-        frame.new(balls: [10, pins])
-      in LastFrame(balls: [ball]) if ball + pins == 10
-        frame.new(balls: [ball, pins])
-      in LastFrame(balls: [ball]) if ball + pins > 10
-        :too_many_pins
-      in LastFrame(balls: balls)
-        frame.new(balls: [*balls, pins], state: 'ended')
       end
     end
   end
@@ -34,29 +40,14 @@ module Bowling
   class Frame < Dry::Struct
     attribute :balls, Types.Array(Types::Integer).default([].freeze)
     attribute :state, Types::String.default('playing').enum('playing', 'ended')
-    attribute :type, Types.Value('regular').default('regular')
-    attribute :serial_number, Types::Integer
+    attribute :serial_number, Types::Integer.constrained(included_in: 1..10)
 
     def strike?
       balls.first == 10
     end
 
     def spare?
-      !strike? && balls.sum == 10
-    end
-
-    def ended?
-      state == 'ended'
-    end
-  end
-
-  class LastFrame < Dry::Struct
-    attribute :balls, Types.Array(Types::Integer).default([].freeze)
-    attribute :state, Types::String.default('playing').enum('playing', 'ended')
-    attribute :type, Types.Value('last').default('last')
-
-    def serial_number
-      10
+      !strike? && balls.take(2).sum == 10
     end
 
     def ended?
@@ -65,7 +56,7 @@ module Bowling
   end
 
   class Game < Dry::Struct
-    attribute :frames, Types.Array(Frame | LastFrame).default([Frame.new(serial_number: 1)].freeze)
+    attribute :frames, Types.Array(Frame).default([Frame.new(serial_number: 1)].freeze)
     attribute :state, Types::String.default('playing').enum('playing', 'ended')
     attribute :player_name, Types::String
     attribute :uid, (Types::UID.default { SecureRandom.hex })
@@ -90,16 +81,16 @@ module Bowling
       *previous_frames, _ = frames
 
       case next_frame_state.call(current_frame, knocked_down_pins)
-      in Frame(state: 'ended') => frame if frames.count == 9
-        new(frames: [*previous_frames, frame, LastFrame.new])
+      # Once the last frame ends, we'll end the whole game
+      in Frame(state: 'ended', serial_number: 10) => frame
+        new(frames: [*previous_frames, frame], state: 'ended')
+      # We add new frames after the previous ones have ended
       in Frame(state: 'ended') => frame
         new(frames: [*previous_frames, frame, Frame.new(serial_number: next_frame_number)])
+      # Just update a frame after a throw
       in Frame => frame
         new(frames: [*previous_frames, frame])
-      in LastFrame(state: 'ended') => frame
-        new(frames: [*previous_frames, frame], state: 'ended')
-      in LastFrame => frame
-        new(frames: [*previous_frames, frame])
+      # Propagate errors
       in Symbol => error
         error
       end

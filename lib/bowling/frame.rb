5a142e387a364789c088b32791a6edfe28f5d527
Igor S. Morozov <igor@morozov.is>
2021-04-29 02:37:19 +0300

    Split game into multiple files (#16)

---
+++ lib/bowling/frame.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'dry-struct'
+
+module Bowling
+  class Frame < Dry::Struct
+    attribute :balls, Types.Array(Types::Integer).default([].freeze)
+    attribute :state, Types::String.default('playing').enum('playing', 'ended')
+    attribute :serial_number, Types::Integer.constrained(included_in: 1..10)
+
+    def strike?
+      balls.first == 10
+    end
+
+    def spare?
+      !strike? && balls.take(2).sum == 10
+    end
+
+    def ended?
+      state == 'ended'
+    end
+  end
+end
